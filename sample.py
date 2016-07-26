0014d4c14f02ceff584b1b9d20d3f12365206d24
Brian Dailey <brian@realm3.com>
2016-07-25 22:50:43 -0500

    First pass.

---
+++ sample.py
@@ -0,0 +1,86 @@
+import random
+import click
+import boto3
+
+# How many bytes should we keep looking for header length?
+MAX_HEADER_OFFSET = 100000
+CR = "\r\n"
+
+
+def get_s3_client():
+    """ Simply return a boto s3 client. """
+    return boto3.client('s3')
+
+
+def get_headers(client, bucket, key, delimiter, sample_bytes=1000):
+    """ Try to get the header of an object. """
+    header_sample = ""
+    offset = 0
+    while True:
+        header_sample += get_sample(client, bucket, key, offset=offset, sample_bytes=sample_bytes)
+        if CR in header_sample:
+            break
+        elif offset > MAX_HEADER_OFFSET:
+            raise ValueError('Could not find a line break in the first {} bytes.'.format(sample_bytes + offset))
+
+        offset = offset + sample_bytes
+
+    header_str = header_sample[:header_sample.index(CR)]
+
+    return header_str
+
+
+def get_sample(client, bucket, key, offset=0, sample_bytes=1000):
+    response = client.get_object(Bucket=bucket, Key=key, Range='bytes={}-{}'.format(offset, offset + sample_bytes))
+    body_string = response['Body'].read()
+
+    # We don't want binary. Give me something useful, like unicode.
+    body_string = body_string.decode('utf8')
+
+    return body_string
+
+
+@click.command()
+@click.option('--bucket', prompt='bucket name', help='bucket name')
+@click.option('--key', prompt='key name', help='key name')
+@click.option('--headers/--no-headers', default=True, help='Include headers?')
+@click.option('--delimiter', '-d', default=',', help='File delimiter')
+@click.option('--lines', '-l', default=1000, type=int, help='How many sample lines do you want?')
+def main(bucket, key, headers, delimiter, lines):
+    client = get_s3_client()
+
+    head_object = client.head_object(Bucket=bucket, Key=key)
+
+    content_length = head_object['ContentLength']
+
+    sample_offset = 0
+    sample_bytes = 10000
+
+    header_row = get_headers(client, bucket, key, delimiter)
+
+    number_of_fields = len(header_row.split(delimiter))
+
+    if headers:
+        # Because this is included,
+        # we need to make sure we do not include this in our sample.
+        sample_offset = len(header_row.encode('utf8'))
+
+    sample_lines = 0
+
+    while sample_lines < lines:
+        # Grab a sample
+        random_offset = random.randrange(sample_offset, content_length - sample_bytes)
+        sample = get_sample(client,
+            bucket,
+            key,
+            offset=random_offset,
+            sample_bytes=sample_bytes)
+        for line in sample.split(CR):
+            if len(line.split(delimiter)) == number_of_fields:
+                print(line)
+                sample_lines += 1
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
